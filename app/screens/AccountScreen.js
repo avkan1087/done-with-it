198186f625b8902b37570f2d7c5ccd1bbadfed3f
avi <avkan1087@gmail.com>
2020-08-03 16:32:38 -0400

    improvements

--- app/screens/AccountScreen.js
+++ app/screens/AccountScreen.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { StyleSheet, View, FlatList } from "react-native";
 
-import Screen from "../components/Screen";
 import { ListItem, ListItemSeparator } from "../components/lists";
 import colors from "../config/colors";
 import Icon from "../components/Icon";
+import Screen from "../components/Screen";
 
 const menuItems = [
    {
@@ -37,7 +37,7 @@ function AccountScreen(props) {
             <FlatList
                data={menuItems}
                keyExtractor={(menuItem) => menuItem.title}
-               ItemSeparatorComponent={ListItemSeparatorComponent}
+               ItemSeparatorComponent={ListItemSeparator}
                renderItem={({ item }) => (
                   <ListItem
                      title={item.title}
